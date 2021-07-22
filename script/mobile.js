ce4adb525f7a2d264b9a2ea5ea47ad9935efb4a9
cuffourvol2 <81504713+cuffourvol2@users.noreply.github.com>
2021-07-22 20:53:39 +0700

    Add files via upload

--- script/mobile.js
+++ script/mobile.js
@@ -29,3 +29,12 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// init Masonry
+var $grid = $('.grid').masonry({
+  // options...
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
